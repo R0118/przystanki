b44c31a3ef647dac4f6adfc462f2a98a43eca771
M7770 <167993494+R0118@users.noreply.github.com>
2025-01-10 11:28:41 +0100

    final commit

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var mymap = L.map('map', { maxZoom: 25 }).setView([52.340554, 20.950055], 13);
+var mapa = L.map('map', { maxZoom: 25 }).setView([52.317245376694935, 20.957449076417298], 13);
 
 var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-var geoportal = L.tileLayer.wms('https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution', {
-    layers: 'ORTOFOTOMAPA',
-    format: 'image/png',
-    transparent: true,
-    crs: L.CRS.EPSG3857, // Właściwy układ współrzędnych
-    version: '1.3.0', // Wersja WMS 1.3.0
-    attribution: 'Geoportal 2025 - www.geoportal.gov.pl'
-});
-OpenStreetMap.addTo(mymap);
-geoportal.addTo(mymap);
+var Geoportal = L.tileLayer.wms('https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/HighResolution', {
+    layers: 'Raster', format: 'image/png', transparent: true });
+var Terrestris = L.tileLayer.wms('https://ows.terrestris.de/osm/service', {
+    layers: 'OSM-WMS', format: 'image/png', transparent: true });
+OpenStreetMap.addTo(mapa);
 
 var trasy = {};
 var trasywarstw = {};
@@ -18,9 +13,9 @@ var trasywarstw = {};
 Promise.all([
     fetch('./dane.geoJSON').then(res => res.json())
 ]).then(([dane]) => {
-    dane.collections.forEach((collection) => {
-        var nazwa = collection.name;
-        var trasa = collection.features.map(function (feature) {
+    dane.collections.forEach((kolekcja) => {
+        var nazwa = kolekcja.name;
+        var trasa = kolekcja.features.map(function (feature) {
             var w = feature.geometry.coordinates;
             return L.latLng(w[0], w[1]);
         });
@@ -30,7 +25,7 @@ Promise.all([
             router: L.Routing.graphHopper('f4b21e7b-83d4-4e5e-8965-6685f1eb1f5f'),
             createMarker: function (i, waypoint, n) {
                 var iconUrl;
-                var label = collection.features[i]?.properties.label;
+                var label = kolekcja.features[i]?.properties.label;
                 if (i === 0) {
                     iconUrl = 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png';
                 } else if (i === n - 1) {
@@ -54,37 +49,45 @@ Promise.all([
         trasywarstw[nazwa] = L.layerGroup([przystanki.getPlan()]);
     });
 
-    L.control.layers({OpenStreetMap, geoportal}, trasywarstw).addTo(mymap);
+    L.control.layers({OpenStreetMap, Geoportal, Terrestris}, trasywarstw).addTo(mapa);
 
-    mymap.on('overlayadd', function (e) {
+    mapa.on('overlayadd', function (e) {
     var nazwa = e.name;
     if (trasywarstw[nazwa]) {
-        trasy[nazwa].addTo(mymap);
-        trasywarstw[nazwa].addTo(mymap); 
+        trasy[nazwa].addTo(mapa);
+        trasywarstw[nazwa].addTo(mapa); 
     }
 });
 
-mymap.on('overlayremove', function (e) {
+mapa.on('overlayremove', function (e) {
     var nazwa = e.name;
     if (trasywarstw[nazwa]) {
         trasy[nazwa].remove();
         trasywarstw[nazwa].remove(); 
     }
-});
-}).catch(error => {
-    console.error('Error loading GeoJSON file:', error);
-});
+});});
 
 
-var customControl = L.Control.extend({
-    onAdd: function (map) {
-        var container = L.DomUtil.create('div', 'leaflet-routing-custom');
-        container.innerHTML = `
-            <div style="padding: 10px; font-size:100%;background-color:black">
+var legenda = L.Control.extend({
+    onAdd: function () {
+        var k = L.DomUtil.create('div', 'leaflet-routing-custom');
+        k.innerHTML = `<div style="padding: 10px; font-size:100%;background-color:black">
                 <h2 style="text-align: center; color: white;">Mapa tras autobusowych w Warszawie</h2>
+                <div style="font-size:120%; margin-left: 130px; margin-bottom: 10px; display: flex; justify-content: space-between; align-items: center;">
+                <span>Przystanek początkowy —</span>
+                <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png" style="width: 20px; height: 34px;" />
+            </div>
+            <div style="font-size:120%; margin-left: 130px; margin-bottom: 10px; display: flex; justify-content: space-between; align-items: center;">
+                <span>Przystanek końcowy —</span>
+                <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png" style="width: 20px; height: 34px;" />
+            </div>
+            <div style="font-size:120%; margin-left: 130px; margin-bottom: 10px; display: flex; justify-content: space-between; align-items: center;">
+                <span>Pozostałe przystanki —</span>
+                <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png" style="width: 20px; height: 34px;" />
+            </div>
             </div>`;
-        return container;
+        return k;
     },
-    onRemove: function (map) {}
+    onRemove: function () {}
 });
-mymap.addControl(new customControl());
+mapa.addControl(new legenda());
