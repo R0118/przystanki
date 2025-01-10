88f228af5cfae5999e23f9eb32d610f0ace2f5de
M7770 <167993494+R0118@users.noreply.github.com>
2025-01-10 10:45:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var mymap = L.map('map', { maxZoom: 25 }).setView([52.340554, 20.950055], 13);
 
 var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var geoportal = L.tileLayer.wms('https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution', {
+    layers: 'ORTOFOTOMAPA',
+    format: 'image/png',
+    transparent: true,
+    crs: L.CRS.EPSG3857, // Właściwy układ współrzędnych
+    version: '1.3.0', // Wersja WMS 1.3.0
+    attribution: 'Geoportal 2025 - www.geoportal.gov.pl'
+});
 OpenStreetMap.addTo(mymap);
+geoportal.addTo(mymap);
 
 var trasy = {};
 var trasywarstw = {};
@@ -18,8 +27,7 @@ Promise.all([
         var przystanki = L.Routing.control({
             waypoints: trasa,
             language: 'pl',
-            router: new L.Routing.openrouteservice(
-                '5b3ce3597851110001cf62489601ac3aba474ff2accd6be5ebc4ecc8'),
+            router: L.Routing.graphHopper('f4b21e7b-83d4-4e5e-8965-6685f1eb1f5f'),
             createMarker: function (i, waypoint, n) {
                 var iconUrl;
                 var label = collection.features[i]?.properties.label;
@@ -46,7 +54,7 @@ Promise.all([
         trasywarstw[nazwa] = L.layerGroup([przystanki.getPlan()]);
     });
 
-    L.control.layers({}, trasywarstw).addTo(mymap);
+    L.control.layers({OpenStreetMap, geoportal}, trasywarstw).addTo(mymap);
 
     mymap.on('overlayadd', function (e) {
     var nazwa = e.name;
@@ -59,7 +67,7 @@ Promise.all([
 mymap.on('overlayremove', function (e) {
     var nazwa = e.name;
     if (trasywarstw[nazwa]) {
-        //trasy[nazwa].remove();
+        trasy[nazwa].remove();
         trasywarstw[nazwa].remove(); 
     }
 });
@@ -72,9 +80,8 @@ var customControl = L.Control.extend({
     onAdd: function (map) {
         var container = L.DomUtil.create('div', 'leaflet-routing-custom');
         container.innerHTML = `
-            <div style="padding: 40px; font-size:100%;background-color:black">
-                <h1 style="text-align: center; color: white;">SKIBIDI</h1>
-                <p style="text-align: center; color: white;">DIS</p>
+            <div style="padding: 10px; font-size:100%;background-color:black">
+                <h2 style="text-align: center; color: white;">Mapa tras autobusowych w Warszawie</h2>
             </div>`;
         return container;
     },
